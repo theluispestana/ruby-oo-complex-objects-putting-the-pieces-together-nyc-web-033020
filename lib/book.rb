5bb740e39dd636c5ac313bbbb566a4c6921eccaa
theluispestana <theluispestana@gmail.com>
2020-03-31 16:21:21 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
-
+class Book 
+  attr_accessor :title
+  attr_accessor :page_count
+  attr_accessor :genre
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def author
+    @author 
+  end
+  def author=(author)
+    @author = author
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
